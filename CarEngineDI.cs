05183fa6f88ed963585061f6cbadcbf07a71c02e
Rafael Carlo André de Queiroz <rafaelqueiroz@msn.com>
2017-09-22 21:09:03 -0300

    Car / Engine / Log example

---
+++ CarEngineDI.cs
@@ -0,0 +1,78 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Dependency_Injection
+{
+    public interface ILog
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLog : ILog
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Engine
+    {
+        private ILog log;
+        private int id;
+
+        public Engine(ILog log)
+        {
+            this.log = log;
+            id = new Random().Next();
+        }
+
+        public void Ahead(int power)
+        {
+            log.Write($"Engine [{id}] ahead {power}");
+        }
+    }
+
+    public class Car
+    {
+        private Engine engine;
+        private ILog log;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.engine = engine;
+            this.log = log;
+        }
+
+        public void Go()
+        {
+            engine.Ahead(100);
+            log.Write("Car going forward...");
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleLog>().As<ILog>().AsSelf(); //registred ConsoleLog with ILog and ConsoleLog without the ILog (.AsSelf() method)
+            builder.RegisterType<Engine>();
+            builder.RegisterType<Car>();
+
+            IContainer container = builder.Build();
+
+            var log = container.Resolve<ConsoleLog>();
+
+            var car = container.Resolve<Car>();
+            car.Go();
+        }
+    }
+}
---
+++ CarEngineWithoutDI.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Dependency_Injection
+{
+    /// <summary>
+    /// This example shows a particular problem, we have an interface ILog
+    /// A class called ConsoleLog which inherits from ILog to log to the console
+    /// An Engine class
+    /// And a Car class which depends on the Engine to initialize, all goes well but what if we had more
+    /// dependencies in the constructor? The code would be a lot bigger and harder to read. So DI here is handy. 
+    /// </summary>
+    class CarEngineWithoutDI
+    {
+        public interface ILog
+        {
+            void Write(string message);
+        }
+
+        public class ConsoleLog : ILog
+        {
+            public void Write(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public class Engine
+        {
+            private ILog log;
+            private int id;
+
+            public Engine(ILog log)
+            {
+                this.log = log;
+                id = new Random().Next();
+            }
+
+            public void Ahead(int power)
+            {
+                log.Write($"Engine [{id}] ahead {power}");
+            }
+        }
+
+        public class Car
+        {
+            private Engine engine;
+            private ILog log;
+
+            public Car(Engine engine, ILog log)
+            {
+                this.engine = engine;
+                this.log = log;
+            }
+
+            public void Go()
+            {
+                engine.Ahead(100);
+                log.Write("Car going forward...");
+            }
+        }
+
+        internal class Program
+        {
+            //public static void Main(string[] args)
+            //{
+            //    var log = new ConsoleLog();
+            //    var engine = new Engine(log);
+            //    var car = new Car(engine, log);
+            //    car.Go();
+            //}
+        }
+    }
+}
